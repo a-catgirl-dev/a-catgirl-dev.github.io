0083ea3c8ba199aa5c768ea21bf7596071a10bfd
a catgirl <nyameowmeow@duck.com>
2025-05-17 15:34:48 -0400

    feat: don't always play audio

--- js/main.rs.js
+++ js/main.rs.js
@@ -8,26 +8,36 @@ const overlay = document.getElementById("wait-for-input");
 const mainContent = document.getElementById("totally-real-app-mount-react-native-winjs-why-do-people-even-do-this");
 const audio = document.getElementById("audio");
 
-overlay.addEventListener("click", () => {
-    setTimeout(function() {
-        var o = 0;
-        var timer = setInterval(function() {
-            if (o >= 1.5) {
-                clearInterval(timer);
-            }
-            mainContent.style.opacity = o;
-            o += 0.01;
+let done = false;
 
-            // bad and stupid but im too lazy to figure out how to wait until the timer is over
-            mainContent.classList.remove("hidden");
-        }, 5);
-    }, 0);
+function disableAudio() {
+    done = true;
+    console.log("meow");
+    showMainSite();
+}
+// i hate jhavascript so much
+window.disableAudio = disableAudio;
+
+function showMainSite() {
+    var o = 0;
+    var timer = setInterval(function() {
+        if (o >= 1.5) {
+            clearInterval(timer);
+        }
+        mainContent.style.opacity = o;
+        o += 0.01;
+
+        // bad and stupid but im too lazy to figure out how to wait until the timer is over
+        mainContent.classList.remove("hidden");
+    }, 5);
 
     overlay.style.opacity = "0";
     overlay.style.pointerEvents = "none";
 
     overlay.style.display = "none";
+}
 
+function startAudio() {
     audio.volume = 0.5;
     audio.play().then(() => {
         const visualizer = new Visualizer(audio);
@@ -36,4 +46,25 @@ overlay.addEventListener("click", () => {
     }).catch(error => {
         console.log("gg.", error);
     });
+}
+
+function isMobile() {
+    return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+    console.log("p");
+    // poorly named function, but skips the whole click here thing and doens't do audio.
+    disableAudio();
+}
+
+document.body.addEventListener("click", () => {
+    if (done) {
+        // TODO: how do i destroy an eventListener?
+        return;
+    }
+    done = true;
+
+    showMainSite();
+    startAudio();
 });
